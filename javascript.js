6cbcd8e73441d89503d9eb785393833c6f052929
adergoat <97291315+aderguteyt@users.noreply.github.com>
2022-09-23 19:56:34 +0530

    coax

---
+++ javascript.js
@@ -0,0 +1,14 @@
+var firstname = "Sree";
+var lastname = "wait why do you need last name?";
+var full = firstname.concat(lastname)
+console.log(full);
+var a = 154.45855;
+var s = 897.1234567890;
+var add = a + s
+console.log("The addition of the two kinda dumb numbers 154.45855 and 897.1234567890 is " + add);
+var sub = a - s
+console.log("The subtraction of the two kinda dumb numbers 154.45855 and 897.1234567890 is" + sub);
+var mul = a * s
+console.log("The product of the two kinda dumb numbers 154.45855 and 897.1234567890 is" + mul);
+var div = a / s
+console.log("The division of the two kinda dumb numbers 154.45855 and 897.1234567890 is" + div)
